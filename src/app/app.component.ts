358b0b47b4d972149edb882ec71c367d6b5b5073
john@work <johninquest@gmail.com>
2022-09-16 19:46:36 +0200

    added basic google analytics to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { from } from 'rxjs';
+
+declare const gtag: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(_router: Router) {
+    const navEndEvent$ = _router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: any) => {
+      gtag('config', 'G-RYMEE5S8LR', { page_path: e.urlAfterRedirects });
+    });
+  }
+}
